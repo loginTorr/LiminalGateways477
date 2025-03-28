4cb2ed78d9d6eaa60622c9d0d8d1dac930fd99a1
gabethomp <gabealan9@gmail.com>
2025-03-27 20:24:29 -0500

    notes from day 1 class

---
+++ Assets/scripts/Door.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using State = DoorState
+
+public enum DoorState {
+    CLOSED,
+    OPENING,
+    OPEN,
+}
+
+public class Door : MonoBehaviour{
+    private Dictionary<DoorState, Action> StateMethods;
+
+    private DoorState CurState { get; private set; }
+
+    // Start is called before the first frame update
+    void Start() {
+        StateMethods = new {    
+            [DoorState.CLOSED] = StateClosed,
+            [DoorState.OPENING] = StateOpening,
+            [DoorState.OPEN] = StateOpen,
+        };
+        CurState = DoorState.CLOSED;
+    }
+
+
+    // Update is called once per frame
+    void Update(){ 
+        if (StateMethods.ContainsKey(CurState)) {
+            StateMethods[CurState]();
+        }
+    }
+
+    private void StateClosed() {
+    }
+
+    private void StateOpening() {
+    }
+
+    private void StateOpen() {
+    }
+}
