868a1c251f61a9421911b041b8ea7d5a6fd23fcf
loginTorr <ltorr0401@gmail.com>
2025-03-31 02:16:54 -0500

    Lecturn Functionality and object snapping
    Spawns objects based on what book is there

--- Assets/scripts/Lecturn.cs
+++ Assets/scripts/Lecturn.cs
@@ -1,25 +1,48 @@
 using System.Collections;
 using System.Collections.Generic;
+using SerializableCallback;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
 using UnityEngine.XR.Interaction.Toolkit.Interactables;
+using UnityEngine.XR.Interaction.Toolkit.Interactors;
 
 
 public class Lecturn : MonoBehaviour
 {
-    private lecturnState curBook;
+
     public Game gameScript;
+
+    public lecturnState curBook;
+
+    public GameObject NatureBook;
+    public GameObject VideoBook;
+    public GameObject StarBook;
+
+    public GameObject NatureScene;
+    public GameObject VideoScene;
+    public GameObject StarScene;
+
     private XRBaseInteractable interactable;
+    private XRSocketInteractor socket;
 
 
 
+
+    void Awake()
+    {
+        // Grabs references, ensuring they're set before OnEnable
+        gameScript = FindObjectOfType<Game>();
+        interactable = GetComponent<XRBaseInteractable>();
+        socket = GetComponent<XRSocketInteractor>();
+
+    }
 
     // Start is called before the first frame update
     void Start()
     {
         curBook = lecturnState.inactive;
-        gameScript = FindObjectOfType<Game>(); //grabs objects from game script
-        interactable = GetComponent<XRBaseInteractable>();
+
     }
 
     // Update is called once per frame
@@ -34,7 +57,6 @@ public class Lecturn : MonoBehaviour
 
             //continously calls a function to check for the books, switches to specific book state when conditions met
             case lecturnState.CheckBook:
-                ifBook();
                 break;
 
             case lecturnState.NatureBook:
@@ -51,11 +73,57 @@ public class Lecturn : MonoBehaviour
         
     }
 
-    public void ifBook()
-    {
-        Debug.Log("Works!");
-        curBook = lecturnState.StarBook;
+    private void OnSocketSelectEntered(SelectEnterEventArgs args)
+    { 
+        // Retrieve the snapped object's GameObject.
+        GameObject book = args.interactableObject.transform.gameObject;
+
+        if (book.name.Contains("NatureBook"))
+        {
+            NatureScene.SetActive(true);
+        }
+      
+        if (book.name.Contains("VideoBook"))
+        {
+            VideoScene.SetActive(true);
+        }
     }
 
+    private void OnSocketSelectExited(SelectExitEventArgs args)
+    {
+        // Retrieve the snapped object's GameObject.
+        GameObject book = args.interactableObject.transform.gameObject;
+
+        if (book.name.Contains("NatureBook"))
+        {
+            NatureScene.SetActive(false);
+        }
+
+        if (book.name.Contains("VideoBook"))
+        {
+            VideoScene.SetActive(false);
+        }
+    }
+
+    private void OnEnable()
+    {
+        // Subscribe to the socket's event.
+        if (socket != null)
+        {
+            socket.selectEntered.AddListener(OnSocketSelectEntered);
+            socket.selectExited.AddListener(OnSocketSelectExited);
+        }
+
+    }
+
+    private void OnDisable()
+    {
+        // Unsubscribe when the object is disabled.
+        if (socket != null)
+        {
+            socket.selectEntered.RemoveListener(OnSocketSelectEntered);
+            socket.selectExited.RemoveListener(OnSocketSelectExited);
+        }
+    }
 
 }
